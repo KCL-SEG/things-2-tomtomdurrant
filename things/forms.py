5236e4c5585b2af3c5cf6d5d3e4d6559c2bdcef2
Tom Durrant <tomchdurrant@gmail.com>
2023-12-08 14:09:04 +0000

    Add validation boogaloo

--- things/forms.py
+++ things/forms.py
@@ -9,5 +9,5 @@ from .models import *
 class ThingForm(forms.Form):
     """Form for creating a Thing."""
     name = forms.CharField(max_length=35)
-    description = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(widget=forms.Textarea, max_length=120)
     quantity = forms.IntegerField(min_value=0, max_value=50)
