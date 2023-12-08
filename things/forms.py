2185987ff47ed594abc7709737286fb80b3bbf3a
Tom Durrant <tomchdurrant@gmail.com>
2023-12-08 14:03:40 +0000

    Test things2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+class ThingForm(forms.Form):
+    """Form for creating a Thing."""
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField()
