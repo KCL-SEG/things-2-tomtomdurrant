2c5c0c0127afc8d6b25b42bce2e5f0390c3fa3d4
Tom Durrant <tomchdurrant@gmail.com>
2023-12-08 14:07:52 +0000

    Add validation

--- things/views.py
+++ things/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            # Do something with the form data
+            pass
     return render(request, 'home.html', {'form': ThingForm})
